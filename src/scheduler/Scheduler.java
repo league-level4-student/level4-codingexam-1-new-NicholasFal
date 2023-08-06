315785d7d15d6a3082056a367d684fa8cad1a7e8
root <root@c3c1637cee9b>
2023-08-06 19:57:13 +0000

    I am almost done with level 4!

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -28,31 +28,96 @@ import scheduler.Event.Days;
  *   variables.
  */
 public class Scheduler {
-
-    public static void main(String[] args) {
+	static String describe;
+	static String time;
+	static String day;
+    public static void main(String[] args) throws SchedulingConflictException {
     	Days[] daysWeek = Event.Days.values();
     	LinkedList<Event> events = new LinkedList<Event>();
+    	while(true) {
     	Scanner scan = new Scanner(System.in);
     	System.out.println("Would you like to add an event, view events, or remove an event? (add, view, remove)");
     	String choice = scan.next();
     	if(choice.equalsIgnoreCase("add")) {
-    		
+    		addEvent();
+    		validSlot(events, Integer.parseInt(time), Days.valueOf(day.toUpperCase()));
+    		events.add(new Event(describe, Integer.parseInt(time), Days.valueOf(day.toUpperCase())));
     	} else if(choice.equalsIgnoreCase("view")) {
-    		
+    		viewEvent(events);
     	} else if(choice.equalsIgnoreCase("remove")) {
-    		
+    		removeEvent();
+    		removeEventsTwo(events, describe, Integer.parseInt(time), Days.valueOf(day.toUpperCase()));
     	}
+    }
     }
     public static void addEvent() {
     	System.out.println("What event would you like to add?");
     	Scanner scan = new Scanner(System.in);
-    	String describe = scan.next();
+    	describe = scan.next();
     	System.out.println("What time would you like to add the event? (hour 0 - 23)");
-    }
-    public static void viewEvent() {
+    	Scanner scanTwo = new Scanner(System.in);
+    	time = scanTwo.next();
     	
+    	System.out.println("What day would you like to add the event?");
+    	Scanner scanThree = new Scanner(System.in);
+    	day = scanThree.next();
+    }
+    public static void viewEvent(LinkedList events) {
+    	Node current = events.getHead();
+    	Node index;
+    	while(current != null) {
+    		index = current.getNext();
+    		while(index != null) {
+    		Event currentE = (Event) current.getValue();
+    		Event indexE = (Event) index.getValue();
+    		if(currentE.getTime() > indexE.getTime()) {
+    			int temp = currentE.getTime();
+    			currentE.time = indexE.time;
+    			indexE.time = temp;
+    		}
+    		index = index.getNext();
+    		}
+        	current = current.getNext();
+    	}
+    	LinkedList<Event> newEvents = new LinkedList<Event>();
+    	newEvents = events;
     }
     public static void removeEvent() {
+    	System.out.println("What event would you like to delete?");
+    	Scanner scan = new Scanner(System.in);
+    	describe = scan.next();
+    	System.out.println("What time was this event? (hour 0 - 23)");
+    	Scanner scanTwo = new Scanner(System.in);
+    	time = scanTwo.next();
+    	System.out.println("What day was this event?");
+    	Scanner scanThree = new Scanner(System.in);
+    	day = scanThree.next();
     	
+    }
+    private static void validSlot(LinkedList events, Integer time, Days day) throws SchedulingConflictException {
+    	LinkedList<Event> newEvents = new LinkedList<Event>();
+    	for(int i = 0; i < events.size(); i++) {
+        	Event head = (Event) events.getHead().getValue();
+    		if(head.getDay() == day && head.getTime() == time) {
+    			throw new SchedulingConflictException("You tried to book a double time slot!");
+    		} else {
+    			newEvents.add(head);
+    		}
+    		events.remove(0);
+    	}
+    	events = newEvents;
+    }
+    private static void removeEventsTwo(LinkedList events, String describer, Integer time, Days day) {
+    	LinkedList<Event> newEvents = new LinkedList<Event>();
+    	for(int i = 0; i < events.size(); i++) {
+        	Event head = (Event) events.getHead().getValue();
+    		if(head.getDay() == day && head.getTime() == time && head.getDescriber() == describer) {
+    		//Here, the head will not be added to newEvents, so it will end up being removed
+    		} else {
+    			newEvents.add(head);
+    		}
+    		events.remove(0);
+    	}
+    	events = newEvents;
     }
 }
---
+++ src/scheduler/SchedulingConflictException.java
@@ -0,0 +1,9 @@
+package scheduler;
+
+public class SchedulingConflictException extends Exception {
+
+	public SchedulingConflictException(String string) {
+		super(string);
+	}
+
+}
